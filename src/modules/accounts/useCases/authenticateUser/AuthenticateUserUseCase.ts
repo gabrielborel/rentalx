63a926236fb2ad430a81f9269bdec171aee329dd
Gabriel <biel_borel@hotmail.com>
2022-08-28 16:40:48 -0300

    Authenticate User useCase created

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,53 @@
+import { inject, injectable } from "tsyringe";
+import { IUserRepository } from "../../repositories/IUsersRepository";
+import { compare } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import { AppError } from "../../../../errors/AppError";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  }
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUserRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) throw new AppError("Email or password incorrect");
+    
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) throw new AppError("Email or password incorrect");
+
+    const token = sign({}, "6133fa31052080b27cedcade6fb17d49", {
+      subject: user.id,
+      expiresIn: "1d"
+    });
+
+    const tokenReturn: IResponse = {
+      user: {
+        name: user.name,
+        email: user.email
+      },
+      token
+    }
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase }
