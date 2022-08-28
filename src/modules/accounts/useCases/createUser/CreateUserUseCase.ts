ec29a48cda6b7a4f630726b1aa311fd375a8c037
Gabriel <biel_borel@hotmail.com>
2022-08-28 16:40:37 -0300

    Create User useCase created

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from "tsyringe";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUserRepository } from "../../repositories/IUsersRepository";
+import { hash} from 'bcrypt'
+import { AppError } from "../../../../errors/AppError";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUserRepository
+  ) {}
+
+  async execute({ name, email, driver_license, password }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) throw new AppError("User already exists");
+    
+    const passwordHash = await hash(password, 8);
+
+    await this.usersRepository.create({ 
+      name, 
+      email, 
+      driver_license, 
+      password: passwordHash 
+    });
+  }
+}
+
+export { CreateUserUseCase }
