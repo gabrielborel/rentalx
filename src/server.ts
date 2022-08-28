e9556d1e03fa81b8116ea6965773017edea01da0
Gabriel <biel_borel@hotmail.com>
2022-08-28 09:10:45 -0300

    Added tsyringe to server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
+import "reflect-metadata";
 import express from "express";
 import { router } from "./routes";
 import swaggerUI from "swagger-ui-express";
 import swaggerFile from "./swagger.json";
 import './database';
+import './shared/container'
 
 const $PORT = 3333;
 const app = express();
