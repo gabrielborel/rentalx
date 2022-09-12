790d68792aa7c03bc0df191bba02cafb50c06668
Gabriel Borel <biel_borel@hotmail.com>
2022-09-12 12:45:50 -0300

    FindAllAvailablesCar method added to the in-memory cars repository

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -18,6 +18,25 @@ class CarsRepositoryInMemory implements ICarsRepository {
     const car = this.cars.find((car) => car.license_plate === license_plate);
     return car;
   }
+
+  async findAllAvailables(
+    brand?: string,
+    category_id?: string,
+    name?: string
+  ): Promise<Car[]> {
+    const availableCars = this.cars.filter((car) => {
+      if (
+        car.available === true ||
+        (brand && car.brand === brand) ||
+        (category_id && car.category_id === category_id) ||
+        (name && car.name === name)
+      ) {
+        return car;
+      }
+      return null;
+    });
+    return availableCars;
+  }
 }
 
 export { CarsRepositoryInMemory };
