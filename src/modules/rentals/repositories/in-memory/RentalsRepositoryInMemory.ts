b6f86a443d7005ce34978776682eff89670f3211
Gabriel Borel <biel_borel@hotmail.com>
2022-09-14 00:15:23 -0300

    RentalsRepository in-memory repositorie created for tests purposes

---
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -0,0 +1,42 @@
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "../IRentalsRepository";
+
+class RentalsRepositoryInMemory implements IRentalsRepository {
+  rentals: Rental[] = [];
+
+  async findOpenRentalByCarId(car_id: string): Promise<Rental> {
+    const rental = this.rentals.find(
+      (rental) => rental.car_id === car_id && !rental.end_date
+    );
+    return rental;
+  }
+
+  async findOpenRentalByUserId(user_id: string): Promise<Rental> {
+    const rental = this.rentals.find(
+      (rental) => rental.user_id === user_id && !rental.end_date
+    );
+    return rental;
+  }
+
+  async create({
+    car_id,
+    expected_return_date,
+    user_id,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const rental = new Rental();
+
+    Object.assign(rental, {
+      car_id,
+      user_id,
+      expected_return_date,
+      start_date: new Date(),
+    });
+
+    this.rentals.push(rental);
+
+    return rental;
+  }
+}
+
+export { RentalsRepositoryInMemory };
