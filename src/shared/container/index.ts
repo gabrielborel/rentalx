2d18e1356b3c14911e97c021f2a5c4e5e6646c3b
Gabriel Borel <biel_borel@hotmail.com>
2022-09-14 00:58:59 -0300

    Added RentalsRepository to singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,5 @@
 import { container } from "tsyringe";
+import "@shared/container/providers";
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUserRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
@@ -9,6 +10,8 @@ import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { ICarsImageRepository } from "@modules/cars/repositories/ICarsImageRepository";
 import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { RentalsRepository } from "@modules/rentals/infra/typeorm/repositories/RentalsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -25,9 +28,14 @@ container.registerSingleton<IUserRepository>(
   UsersRepository
 );
 
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+
 container.registerSingleton<ICarsImageRepository>(
   "CarsImagesRepository",
   CarsImagesRepository
 );
 
-container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+container.registerSingleton<IRentalsRepository>(
+  "RentalsRepository",
+  RentalsRepository
+);
