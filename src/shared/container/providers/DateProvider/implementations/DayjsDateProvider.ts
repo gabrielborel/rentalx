faf8bb3bc7a7a675fbf15f36a78f3d4f1b038821
Gabriel Borel <biel_borel@hotmail.com>
2022-09-14 00:59:19 -0300

    Date provider created

---
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -0,0 +1,23 @@
+import { IDateProvider } from "../IDateProvider";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+
+class DayJsDateProvider implements IDateProvider {
+  compareInHours(start_date: Date, end_date: Date): number {
+    const end_date_UTC = this.convertToUTC(end_date);
+    const start_date_UTC = this.convertToUTC(start_date);
+    return dayjs(end_date_UTC).diff(start_date_UTC, "hours");
+  }
+
+  convertToUTC(date: Date): string {
+    return dayjs(date).utc().local().format();
+  }
+
+  dateNow(): Date {
+    return dayjs().toDate();
+  }
+}
+
+export { DayJsDateProvider };
