356ecf953eb7f9240a1ed1fd6cc4e5a7a20f5bc7
Gabriel Borel <biel_borel@hotmail.com>
2022-09-19 22:14:56 -0300

    Added listRentalsByUser route to Rentals routes

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -2,16 +2,23 @@ import { Router } from "express";
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { ReturnRentalController } from "@modules/rentals/useCases/returnRental/ReturnRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 
 const rentalRoutes = Router();
 const createRentalController = new CreateRentalController();
 const returnRentalController = new ReturnRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
 rentalRoutes.post(
   "/return/:id",
   ensureAuthenticated,
   returnRentalController.handle
+);
+rentalRoutes.get(
+  "/user",
+  ensureAuthenticated,
+  listRentalsByUserController.handle
 );
 
 export { rentalRoutes };
