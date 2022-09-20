6e5b57e068f159796382dbed49e7d021f66be1f8
Gabriel Borel <biel_borel@hotmail.com>
2022-09-19 21:31:19 -0300

    Added returnRental route to rentals routes

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { ReturnRentalController } from "@modules/rentals/useCases/returnRental/ReturnRentalController";
 
 const rentalRoutes = Router();
 const createRentalController = new CreateRentalController();
+const returnRentalController = new ReturnRentalController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+rentalRoutes.post(
+  "/return/:id",
+  ensureAuthenticated,
+  returnRentalController.handle
+);
 
 export { rentalRoutes };
